179a4391adba195e18b6ce2776860ee71b275378
Abhiram <krisabh12sep@gmail.com>
2026-02-20 03:14:41 +0530

    updated main.py with 5 msg count

--- app/main.py
+++ app/main.py
@@ -176,7 +176,7 @@ def honeypot(payload: Optional[Any] = Body(None), x_api_key: str = Header(None))
         engagement_complete = (
             scam_detected is True
             and extracted_intelligence is not None
-            and get_message_count(session_id) >= 17
+            and get_message_count(session_id) >= 5
             and any(
                 value
                 for key, value in extracted_intelligence.items()
